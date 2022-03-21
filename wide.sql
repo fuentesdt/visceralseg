83c3df3d820b9fef42390d949841a1408f7f2b39
David Fuentes <fuentesdt@gmail.com>
2022-03-21 13:58:52 -0500

    WIP: badawy db update

--- wide.sql
+++ wide.sql
@@ -1,12 +1,14 @@
 .mode csv
 .import nashvisceral/cases.csv    cases
 .import nashvisceral/controls.csv controls
+.import nashvisceral/nashfattycontrol.csv newdb
 
 create table nashimaging  as
-select 'cases' status ,mrn,seriesUID,studyUID from cases where path !="" union
+select 'fatty' status ,mrn,seriesUID,studyUID from cases where path !="" union
 select 'controls' status ,mrn,seriesUID,studyUID from controls where path !="";
+
 .output nashvisceral/wide.csv 
-select *, '/FUS4/IPVL_research/' || mrn ||  '/*/' ||studyUID ||  '/' ||seriesUID path from  nashimaging  ;
+select status ,mrn,seriesUID,studyUID, '/FUS4/IPVL_research/' || mrn ||  '/*/' ||studyUID ||  '/' ||seriesUID path from  newdb;
 -- cat wide.sql  | sqlite3
 .quit
 
